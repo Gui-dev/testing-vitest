194049489fa0a3610b1ee110a7bebe17941a43ad
Gui-dev <adrianon10@gmail.com>
2025-04-24 05:09:04 -0300

    test: it should be able to increment

--- src/hooks/__tests__/use-counter.spec.tsx
+++ src/hooks/__tests__/use-counter.spec.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom/vitest'
 import { describe, expect, it } from 'vitest'
-import { render, renderHook, screen } from '@testing-library/react'
+import { act, renderHook } from '@testing-library/react'
 
 import { useCounter } from '../use-counter'
 
@@ -8,6 +8,24 @@ describe('useCounter())', () => {
   it('should be able to initial value is 5', async () => {
     const { result } = renderHook(() => useCounter({ initialValue: 5 }))
 
-    expect(result.current.count).toEqual(5)
+    expect(result.current.count).toBe(5)
+  })
+
+  it('should be able to increment', async () => {
+    const { result } = renderHook(() => useCounter({ initialValue: 0 }))
+
+    expect(result.current.count).toBe(0)
+
+    act(() => {
+      result.current.increment()
+    })
+
+    expect(result.current.count).toBe(1)
+
+    act(() => {
+      result.current.increment()
+    })
+
+    expect(result.current.count).toBe(2)
   })
 })
